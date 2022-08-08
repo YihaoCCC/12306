09b2b53ff8e69c96c229eec018876bf06483cab6
奕-Sugon <yihao168830@163.com>
2022-08-08 18:24:24 +0800

    ✨ feat(NewBranch): 组件抽离

--- src/views/home/homehttp.ts
+++ src/views/home/homehttp.ts
@@ -4,7 +4,7 @@ let request = CkHttp.getInstance()
 
 
 export const testapi = () => {
-    return request.get('/mock/a373bc4295742a91ec188c9d769e0f2d/test/login/13').then((res) => {
+    return request.get('/test').then((res) => {
         return res
     }) 
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
